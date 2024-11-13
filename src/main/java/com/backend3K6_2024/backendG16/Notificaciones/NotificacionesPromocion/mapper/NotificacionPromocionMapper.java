4d0ddbeb3ec0d5392caffd2121b22a1f3fdc86e6
emmach02 <emmanuelricardochaile@hotmail.com>
2024-11-12 21:15:59 -0300

    cambios docker, y conexión bbdd

--- src/main/java/com/backend3K6_2024/backendG16/Notificaciones/NotificacionesPromocion/mapper/NotificacionPromocionMapper.java
+++ src/main/java/com/backend3K6_2024/backendG16/Notificaciones/NotificacionesPromocion/mapper/NotificacionPromocionMapper.java
@@ -6,21 +6,11 @@ import com.backend3K6_2024.backendG16.Notificaciones.NotificacionesPromocion.ent
 public class NotificacionPromocionMapper {
     public static NotificacionPromocionDTO toDTO(NotificacionPromocion notProm) {
         return NotificacionPromocionDTO.builder()
-                .idNotProm(notProm.getIdNotProm())
-                .interesado(notProm.getInteresado())
-                .modelo(notProm.getModelo())
-                .fecha(notProm.getFecha())
-                .descripcion(notProm.getDescripcion())
                 .build();
     }
 
     public static NotificacionPromocion toEntity(NotificacionPromocionDTO dto) {
         return NotificacionPromocion.builder()
-                .idNotProm(dto.getIdNotProm())
-                .interesado(dto.getInteresado())
-                .modelo(dto.getModelo())
-                .fecha(dto.getFecha())
-                .descripcion(dto.getDescripcion())
                 .build();
     }
 }
